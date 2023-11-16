859fe0852123f0976da912e2e74ac1791eeb6e7f
SasimaP <jassm10phanta@gmail.com>
2023-11-16 16:28:32 +0700

    Create Sample NAV & Install Bootstrap

---
+++ src/components/ButtonMenu.js
@@ -0,0 +1,10 @@
+import React , { Component } from 'react';
+
+function ButtonMenu () {
+    return (
+        <div className='buttonmenu'>
+            
+        </div>
+        )   
+}
+export default ButtonMenu;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react';
+
+function Footer () {
+    return (
+        <div className='footer'>
+            <div>footer</div>
+        </div>
+    )
+}
+
+export default Footer;
