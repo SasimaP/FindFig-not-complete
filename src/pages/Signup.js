a88ea80e0b28b7580449867b6d75f39e06cc4999
Sasima Phanta <jassm10phanta@gmail.com>
2023-12-02 21:02:04 +0700

    SignUp & Login

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-function Login() {
+function Signup() {
   return (
     <div style={{ maxWidth: '400px', margin: 'auto'}}>
       <form>
@@ -12,18 +12,18 @@ function Login() {
                     borderTopLeftRadius: '2rem',
                     borderTopRightRadius: '0',
                     borderBottomRightRadius: '0',
-                    borderBottomLeftRadius: '2rem', }}
+                    borderBottomLeftRadius: '2rem', 
+                    backgroundColor: '#60ABFB', color: 'white', border: '#60ABFB' }}
               >Sign Up
             </label>
 
             <input type="radio" class="btn-check" name="btnradio" id="Login" autocomplete="off"/>
-            <label class="btn btn-outline-primary" for="Login"
+            <label class="btn btn-light" for="Login"
               style={{ width: '100%', padding: '0.75rem', fontSize: '1rem', 
               borderTopLeftRadius: '0',
               borderTopRightRadius: '2rem',
               borderBottomRightRadius: '2rem',
-              borderBottomLeftRadius: '0', 
-              backgroundColor: '#60ABFB', color: 'white', border: '#60ABFB' }}
+              borderBottomLeftRadius: '0', }}
               >Login</label>
           </div>
 
@@ -43,24 +43,36 @@ function Login() {
             style={{ width: '100%', padding: '0.375rem 0.75rem', fontSize: '1rem', lineHeight: '1.5', borderRadius: '2rem' }}
           />
         </div>
-        <div>
-          <p style={{textAlign: 'right'}}><button type="reset" class="btn btn-link" style={{color: '#60ABFB'}}>ลืมรหัสผ่าน</button></p>
+        <div style={{ marginBottom: '1rem', textAlign: 'center' }}>
+          <input
+            type="confirm-password"
+            className="form-control"
+            placeholder="Confirm Password"
+            style={{ width: '100%', padding: '0.375rem 0.75rem', fontSize: '1rem', lineHeight: '1.5', borderRadius: '2rem' }}
+          />
         </div>
+        <div className="form-check my-3">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="agreeCheckbox"
+          />
+          <label className="form-check-label" htmlFor="agreeCheckbox">
+            ฉันยอมรับข้อตกลง
+          </label>
+        </div>
+
         <button
           className="btn btn-primary"
           type="submit"
           style={{ width: '100%', padding: '0.75rem', fontSize: '1rem', borderRadius: '2rem', backgroundColor: '#60ABFB', color: 'white', border: '#60ABFB' }}
         >
-          Login
+          Sign Up
         </button>
       </form>
     </div>
   );
 };
 
-export default Login;
-
-// AEE1FC
-// 60ABFB
-// 5170FD
-// 4636FC
+export default Signup;
