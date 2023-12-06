6c1a111a1efdb2abb47db3881f42b8fa081cb71d
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2023-12-06 19:10:55 +0700

    Create ShopContext.jsx

---
+++ src/components/ShopContext/ShopContext.jsx
@@ -0,0 +1,16 @@
+import React, {createContext} from "react";
+import data_product from "../db/figuredb";
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) =>{
+    const contextValue = {data_product};
+    console.log("Data Product:", data_product);
+    return(
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+
+    )
+}
+export default ShopContextProvider;
