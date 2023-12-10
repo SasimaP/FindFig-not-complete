69ab96219da6798c6a85f3ac0c98a7bb86e35d22
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2023-12-11 04:17:20 +0700

    Update Ordercomplete.jsx

--- src/pages/Ordercomplete/Ordercomplete.jsx
+++ src/pages/Ordercomplete/Ordercomplete.jsx
@@ -6,7 +6,13 @@ function OrderComplete() {
     return(
         <div id="wrapper-order">
             <header>
-                <Nav topic="Order Complete"></Nav>
+            <Nav topic="Order Complete" 
+            back="https://cdn.discordapp.com/attachments/787359617280770051/1183513820220629092/3.png?ex=65889c23&is=65762723&hm=b608f4b0dafa79e069345c1fd46fb2256b13299db710a859f027b85295bcbe44&"
+            account="https://cdn.discordapp.com/attachments/787359617280770051/1183513819427913778/1.png?ex=65889c23&is=65762723&hm=285dc6cb6f325e96a63deb650d157bb45a6fddce1731e5766d15215239f0685b&"
+            cart="https://cdn.discordapp.com/attachments/787359617280770051/1183513819981549639/2.png?ex=65889c23&is=65762723&hm=db11b00f36b56189d04437dad86744125c2456a9d7e4b7e95cc617f33e2e8efb&"
+            >
+            
+            </Nav>
             </header>
             <main id="main">
                 <div className="check-mark-container-or1">
