193fcd86cc3badfd1a52650a79158b347b3d332d
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2023-12-12 14:47:30 +0700

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -11,12 +11,12 @@ function Popup(props){
             <div className="popup-container setup-content col-p">
                 <div className="popup-inner setup-content col-p">
                     <div className="check-mark-container">
-                        <div class="wrapper-popup col "> <svg class="checkmark-popup" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/> <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                        <div className="wrapper-popup col "> <svg className="checkmark-popup" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/> <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
                             </svg>
                         </div>                        
                     </div>
 
-                    <p className="col"><h2>Your figures have been sent.</h2></p>
+                    <h2 className="col">Your figures have been sent.</h2>
                     <p className="col">Please wait for an email response.</p>
                     <button className="close-popup-btn button-5" onClick={handleClick}>close</button>   
                 </div> 
